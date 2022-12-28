51e5f90c6d226652d55551c80460ea675cb799b1
f80dev <herve.hoareau@f80.fr>
2022-12-28 16:08:11 +0100

    initial commit

--- test_api.py
+++ test_api.py
@@ -1,12 +1,62 @@
-from Cosmos import Cosmos
+from time import sleep
+
 from Osmosis import Osmosis
-from Secret import Secret
+from Pool import Pool
+from Task import Task
+from tools import now
+
+ADDRESSES = {
+	"hack":{
+		"osmosis":"osmo1xrzk0rhn5nq4nlmvcps8n83360ezj0t9ludh3a",
+		"secret":"secret1gcewetsfm006yzvk9r4hmt2jgeur6qc9ntrqay",
+		"cosmos":"cosmos1xrzk0rhn5nq4nlmvcps8n83360ezj0t9h87880"
+	},
+	"safe":{
+		"osmosis":"osmo1gcewetsfm006yzvk9r4hmt2jgeur6qc9e4yek2",
+		"secret":"secret1gcewetsfm006yzvk9r4hmt2jgeur6qc9ntrqay",
+		"cosmos":"cosmos1gcewetsfm006yzvk9r4hmt2jgeur6qc93whfqc"
+	}
+}
 
 
-OSMOSIS_ACCOUNT="osmo1gcewetsfm006yzvk9r4hmt2jgeur6qc9e4yek2"
-SECRET_ACCOUNT="secret1gcewetsfm006yzvk9r4hmt2jgeur6qc9ntrqay"
-COSMOS_ACCOUNT="cosmos1gcewetsfm006yzvk9r4hmt2jgeur6qc93whfqc"
 
-def test_balance(addr=COSMOS_ACCOUNT):
-	rc=Cosmos("https://rpc-cosmoshub.ecostake.com").balance(addr)
+
+def test_batch(to_addr=ADDRESSES["safe"]["osmosis"], private_key:str=PRIV):
+	network=Osmosis("testnet")
+	pool=Pool()
+	pool.add(Task(private_key,to_addr,0.01,now()+3,network,occurence=3))
+	solde0=network.balance(to_addr)
+
+	for i in range(100):
+		n_exec=pool.run()
+		sleep(0.1)
+
+	solde1=network.balance(to_addr)
+
+	network.explorer(network.get_address(private_key))
+
+
+
+
+def test_send(to_addr=ADDRESSES["safe"]["osmosis"], private_key:str=PRIV):
+	net=Osmosis(network="testnet")
+	solde0=net.balance(to_addr)
+	for i in range(10):
+		rc=net.transfer(private_key,to_addr,0.02)
+	sleep(10)
+	solde1=net.balance(to_addr)
+	solde=solde1-solde0
+	print("Solde "+str(solde))
+	assert solde>0
+
+
+
+
+def test_balance(addr=ADDRESSES["hack"]["osmosis"]):
+	#rc=Cosmos("https://rpc-cosmoshub.ecostake.com").balance(addr)
+	#assert rc>=0
+	network=Osmosis(network="testnet")
+	#latest=network.block_latest()
+	#infos=network.node_infos()
+	rc=network.balance(addr)
 	assert rc>=0
